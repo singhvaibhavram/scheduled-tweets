dcc54612c54b17d6e5989813b6a23d060a4675c5
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2022-01-29 17:50:56 +0530

    Flash Messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
     def index
+        #flash[:notice] = "You are a genius."
+        #flash[:success] = "Logged in Succesfully."
+        #flash[:warning] = "You are not logged in."
+        #flash[:alert] = "Invalid Email or Password."        
     end
 end
